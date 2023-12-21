3305ebe86f91620fc58ce4c58b45754aa337bffc
Enmanuel Cruz <rember98cjs@gmail.com>
2023-12-20 19:36:13 -0600

    fifth commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
     req.query.limit = '5';
@@ -7,90 +8,14 @@ exports.aliasTopTours = (req, res, next) => {
     next();
 };
 
-class APIFeatures {
-    constructor(query, queryString) {
-        this.query = query;
-        this.queryString = queryString;
-    }
-
-    filter() {
-        const queryObj = { ...this.queryString };
-        const excludeFields = ['page', 'sort', 'limit', 'fields'];
-        excludeFields.forEach((el) => delete queryObj[el]);
-
-        // 1B) Advanced filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(
-            /\b(gte|gt|lte|lt)\b/g,
-            (match) => `$${match}`,
-        );
-        this.query.find(JSON.parse(queryStr));
-
-        return this;
-    }
-
-    sort() {
-        if (this.queryString.sort) {
-            const sortBy = this.queryString.query.sort.split(',').join(' ');
-            this.query = this.query.sort(sortBy);
-            // sort('price ratingsAverage')
-        } else {
-            this.query = this.query.sort('-createdAt');
-        }
-
-        return this;
-    }
-}
-
 exports.getAllTours = async (req, res) => {
     try {
-        console.log(req.query);
-        // BUILD QUERY
-        // 1A) Filtering
-        // const queryObj = { ...req.query };
-        // const excludeFields = ['page', 'sort', 'limit', 'fields'];
-        // excludeFields.forEach((el) => delete queryObj[el]);
-
-        // // 1B) Advanced filtering
-        // let queryStr = JSON.stringify(queryObj);
-        // queryStr = queryStr.replace(
-        //     /\b(gte|gt|lte|lt)\b/g,
-        //     (match) => `$${match}`,
-        // );
-
-        // let query = Tour.find(JSON.parse(queryStr));
-
-        // 2) Sorting
-        // if (req.query.sort) {
-        //     const sortBy = req.query.sort.split(',').join(' ');
-        //     query = query.sort(sortBy);
-        //     // sort('price ratingsAverage')
-        // } else {
-        //     query = query.sort('-createdAt');
-        // }
-
-        // 3) Field limiting
-        // if (req.query.fields) {
-        //     const fields = req.query.fields.split(',').join(' ');
-        //     query = query.select(fields);
-        // } else {
-        //     query = query.select('-__v');
-        // }
-
-        // 4) Pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-        // page=2&limit=10
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if (skip >= numTours) throw new Error('This page does not exist');
-        }
-
         // EXECUTE QUERY
-        const features = new APIFeatures(Tour.find(), req.query).filter().sort();
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
         const tours = await features.query;
 
         // SEND RESPONSE
@@ -181,3 +106,95 @@ exports.deleteTour = async (req, res) => {
         });
     }
 };
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } },
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    numTours: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' },
+                },
+            },
+            {
+                $sort: { avgPrice: 1 },
+            },
+            // {
+            //     $match: { _id: { $ne: 'EASY' } },
+            // },
+        ]);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year * 1; //2021
+
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates',
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`),
+                    },
+                },
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numTourStarts: { $sum: 1 },
+                    tours: { $push: '$name' },
+                },
+            },
+            {
+                $addFields: { month: '$_id' },
+            },
+            {
+                $project: {
+                    _id: 0,
+                },
+            },
+            {
+                $sort: { numTourStarts: -1 },
+            },
+            {
+                $limit: 6,
+            },
+        ]);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                plan,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+}
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-
-
 const DB = process.env.DATABASE.replace(
     '<PASSWORD>',
     process.env.DATABASE_PASSWORD,
@@ -18,9 +16,6 @@ mongoose
         useFindAndModify: false,
     })
     .then(() => console.log('DB connection successful!'));
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
