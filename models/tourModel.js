9af3dfaebb2cc15f0a523e0229d68f0f41f9e69b
Enmanuel Cruz <rember98cjs@gmail.com>
2023-12-23 14:58:41 -0600

    seventh commit

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -88,6 +89,36 @@ const tourSchema = new mongoose.Schema(
             type: Boolean,
             default: false,
         },
+        startLocation: {
+            // GeoJSON
+            type: {
+                type: String,
+                default: 'Point',
+                enum: ['Point'],
+            },
+            coodinates: [Number],
+            address: String,
+            description: String,
+        },
+        locations: [
+            {
+                type: {
+                    type: String,
+                    default: 'Point',
+                    enum: ['Point'],
+                },
+                coordinates: [Number],
+                address: String,
+                description: String,
+                day: Number,
+            },
+        ],
+        guides: [
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: 'User',
+            },
+        ],
     },
     {
         toJSON: { virtuals: true },
@@ -105,6 +136,14 @@ tourSchema.pre('save', function (next) {
     next();
 });
 
+// tourSchema.pre('save', async function (next) {
+//     const guidesPromises = this.guides.map(
+//         async (id) => await User.findById(id),
+//     );
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
+
 // tourSchema.pre('save', (next) => {
 //     console.log('Will save document...');
 //     next();
@@ -121,6 +160,15 @@ tourSchema.pre(/^find/, function (next) {
     this.find({ secretTour: { $ne: true } });
 
     this.start = Date.now();
+    next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt',
+    });
+
     next();
 });
 
